9874b3cd9612a81a5d7d5f86173884cb477135ba
Zachary Choate <zach.a.choate@gmail.com>
2026-01-06 20:51:25 -0500

    initial caddy setup

--- vm.tf
+++ vm.tf
@@ -1,18 +1,18 @@
-resource digitalocean_droplet exit_node {
-  name    = "do-cloud-exit-node"
-  image   = "ubuntu-24-04-x64"
-  region  = "nyc3"
-  size    = "s-1vcpu-1gb"
+resource "digitalocean_droplet" "exit_node" {
+  name   = "do-cloud-exit-node"
+  image  = "ubuntu-24-04-x64"
+  region = "nyc3"
+  size   = "s-1vcpu-1gb"
 
   user_data = templatefile("${path.module}/cloud-init.yaml", {
     # tailscale_auth_key = var.tailscale_auth_key
     tailscale_auth_key = tailscale_tailnet_key.exit_node.key
-    ssh_public_key    = var.ssh_public_key
-    vm_username       = var.vm_username
+    ssh_public_key     = var.ssh_public_key
+    vm_username        = var.vm_username
   })
 }
 
-resource digitalocean_firewall default_outbound {
+resource "digitalocean_firewall" "default_outbound" {
   name        = "default-outbound"
   droplet_ids = [digitalocean_droplet.exit_node.id]
 
@@ -34,13 +34,36 @@ resource digitalocean_firewall default_outbound {
   }
 }
 
-resource digitalocean_firewall tailscale_inbound {
+resource "digitalocean_firewall" "tailscale_inbound" {
   name        = "tailscale-inbound"
   droplet_ids = [digitalocean_droplet.exit_node.id]
 
   inbound_rule {
-    protocol              = "udp"
-    port_range            = "41641"
-    source_addresses      = ["0.0.0.0/0", "::/0"]
-  } 
+    protocol         = "udp"
+    port_range       = "41641"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+}
+
+resource "digitalocean_firewall" "http_inbound" {
+  name        = "http-inbound"
+  droplet_ids = [digitalocean_droplet.exit_node.id]
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "80"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "443"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "udp"
+    port_range       = "443"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
 }
